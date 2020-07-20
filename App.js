5e62feef575d9dd50343991faed1ac5e62ae2509
jaipreethundal <jhundal2021>
2020-07-20 14:20:32 -0700

    location services

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import getUserLocation from './location_access.js'
+/*in order to use the Text, View, Image, etc. features
+we have to import them from their respective modules
+StatusBar would be imported from 'expo-status-bar'
+React from 'react and etc. for others*/
 
 export default function App() {
+  getUserLocation()
   return (
+    /*styles.<variable_name> means that the formatting is in the
+    variable under the StyleSheet*/
     <View style={styles.container}>
-      <Text>Time to build YourExplore</Text>
+      <Text style = {styles.instructions}>
+        hello
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+/*under here the formatting is done*/
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1, //how much of the screen will be taken up
+    backgroundColor: '#ffff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  },
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3',
+  },
+  buttonText: {
+    textAlign: 'center',
+    padding: 20,
+    color: 'white',
   },
 });
